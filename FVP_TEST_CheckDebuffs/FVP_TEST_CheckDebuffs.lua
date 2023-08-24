93f39f085a229772ddddb6b2afb9b141ec65cc4f
MaxWayborn <55049724+MaxWayborn@users.noreply.github.com>
2023-08-24 23:43:02 +0300

    Update FVP_TEST_CheckDebuffs.lua

--- FVP_TEST_CheckDebuffs/FVP_TEST_CheckDebuffs.lua
+++ FVP_TEST_CheckDebuffs/FVP_TEST_CheckDebuffs.lua
@@ -6,8 +6,8 @@ FVPTEST_DebuffList:SetBackdrop({
     insets = { left = 4, right = 4, top = 4, bottom = 4 }
 })
 FVPTEST_DebuffList:SetPoint('CENTER', UIParent)
-FVPTEST_DebuffList:SetWidth(300)
-FVPTEST_DebuffList:SetHeight(90)
+FVPTEST_DebuffList:SetWidth(106)
+FVPTEST_DebuffList:SetHeight(50)
 FVPTEST_DebuffList:SetMovable(true)
 FVPTEST_DebuffList:EnableMouse(true)
 FVPTEST_DebuffList:RegisterForDrag("LeftButton")
@@ -15,17 +15,17 @@ FVPTEST_DebuffList:SetScript("OnDragStart", function() this:StartMoving() end)
 FVPTEST_DebuffList:SetScript("OnDragStop",  function() this:StopMovingOrSizing() end)
 FVPTEST_DebuffList:Show()
 
-FVPTEST_DebuffList:CreateFontString('FVPTEST_Debuff15', 'OVERLAY', 'GameFontNormalSmall')
-FVPTEST_Debuff15:SetPoint('TOPLEFT', FVPTEST_DebuffList, 4, -8)
+FVPTEST_DebuffList:CreateFontString('FVPTEST_Debuff15', 'OVERLAY', 'GameFontHighlightSmall')
+FVPTEST_Debuff15:SetPoint('TOPLEFT', FVPTEST_DebuffList, 4, -4)
 
-FVPTEST_DebuffList:CreateFontString('FVPTEST_Debuff16', 'OVERLAY', 'GameFontNormalSmall')
-FVPTEST_Debuff16:SetPoint('TOPLEFT', FVPTEST_DebuffList, 4, -28)
+FVPTEST_DebuffList:CreateFontString('FVPTEST_Debuff16', 'OVERLAY', 'GameFontHighlightSmall')
+FVPTEST_Debuff16:SetPoint('TOPLEFT', FVPTEST_Debuff15, 'BOTTOMLEFT')
 
-FVPTEST_DebuffList:CreateFontString('FVPTEST_Debuff17', 'OVERLAY', 'GameFontNormalSmall')
-FVPTEST_Debuff17:SetPoint('TOPLEFT', FVPTEST_DebuffList, 4, -48)
+FVPTEST_DebuffList:CreateFontString('FVPTEST_Debuff17', 'OVERLAY', 'GameFontGreenSmall')
+FVPTEST_Debuff17:SetPoint('TOPLEFT', FVPTEST_Debuff16, 'BOTTOMLEFT')
 
-FVPTEST_DebuffList:CreateFontString('FVPTEST_Debuff18', 'OVERLAY', 'GameFontNormalSmall')
-FVPTEST_Debuff18:SetPoint('TOPLEFT', FVPTEST_DebuffList, 4, -68)
+FVPTEST_DebuffList:CreateFontString('FVPTEST_Debuff18', 'OVERLAY', 'GameFontGreenSmall')
+FVPTEST_Debuff18:SetPoint('TOPLEFT', FVPTEST_Debuff17, 'BOTTOMLEFT')
 
 local f = CreateFrame('FRAME')
 f:RegisterEvent('PLAYER_LOGIN')
@@ -43,16 +43,31 @@ f:SetScript('OnEvent', function()
 	timer:SetScript('OnUpdate', function()
 		if GetTime() >= (this.start + 1) then
 			this.start = this.start + 1
-			--SELECTED_CHAT_FRAME:AddMessage(GetTime())
-			local debuff, debuffStack, debuffType
-			debuff, debuffStack, debuffType = UnitDebuff('target', 15)
-			FVPTEST_Debuff15:SetText('15 - ' .. (debuff or ''))
-			debuff, debuffStack, debuffType = UnitDebuff('target', 16)
-			FVPTEST_Debuff16:SetText('16 - ' .. (debuff or ''))
-			debuff, debuffStack, debuffType = UnitDebuff('target', 17)
-			FVPTEST_Debuff17:SetText('17 - ' .. (debuff or ''))
-			debuff, debuffStack, debuffType = UnitDebuff('target', 18)
-			FVPTEST_Debuff18:SetText('18 - ' .. (debuff or ''))
+                    
+            local debuff, debuffStack, debuffType = UnitDebuff("target", 15)
+            if debuff then
+                FVPTEST_Debuff15:SetText('15 - Debuff detect')
+            else
+                FVPTEST_Debuff15:SetText('15')
+            end
+            debuff, debuffStack, debuffType = UnitDebuff("target", 16)
+            if debuff then
+                FVPTEST_Debuff16:SetText('16 - Debuff detect')
+            else
+                FVPTEST_Debuff16:SetText('16')
+            end
+            debuff, debuffStack, debuffType = UnitDebuff("target", 17)
+            if debuff then
+                FVPTEST_Debuff17:SetText('17 - Debuff detect')
+            else
+                FVPTEST_Debuff17:SetText('17')
+            end
+            debuff, debuffStack, debuffType = UnitDebuff("target", 18)
+            if debuff then
+                FVPTEST_Debuff18:SetText('18 - Debuff detect')
+            else
+                FVPTEST_Debuff18:SetText('18')
+            end
 		end
 	end)
 end)
