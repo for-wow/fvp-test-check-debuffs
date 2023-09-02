7b7d31bb9149fa994807cf651201a91dad81b428
MaxWayborn <55049724+MaxWayborn@users.noreply.github.com>
2023-09-02 11:33:57 +0300

    Version 2

--- FVP_TEST_CheckDebuffs/FVP_TEST_CheckDebuffs.lua
+++ FVP_TEST_CheckDebuffs/FVP_TEST_CheckDebuffs.lua
@@ -1,73 +1,41 @@
-CreateFrame('FRAME', 'FVPTEST_DebuffList', TargetFrame)
-FVPTEST_DebuffList:SetBackdrop({
+CreateFrame('FRAME', 'VPTCD_FrmDebuffs', UIParent)
+VPTCD_FrmDebuffs:SetBackdrop({
     bgFile = 'Interface\\DialogFrame\\UI-DialogBox-Background', 
     edgeFile = 'Interface\\Tooltips\\UI-Tooltip-Border',
     tile = true, tileSize = 16, edgeSize = 16, 
     insets = { left = 4, right = 4, top = 4, bottom = 4 }
 })
-FVPTEST_DebuffList:SetPoint('CENTER', UIParent)
-FVPTEST_DebuffList:SetWidth(106)
-FVPTEST_DebuffList:SetHeight(50)
-FVPTEST_DebuffList:SetMovable(true)
-FVPTEST_DebuffList:EnableMouse(true)
-FVPTEST_DebuffList:RegisterForDrag("LeftButton")
-FVPTEST_DebuffList:SetScript("OnDragStart", function() this:StartMoving() end)
-FVPTEST_DebuffList:SetScript("OnDragStop",  function() this:StopMovingOrSizing() end)
-FVPTEST_DebuffList:Show()
+VPTCD_FrmDebuffs:SetPoint('CENTER', UIParent)
+VPTCD_FrmDebuffs:SetWidth(110)
+VPTCD_FrmDebuffs:SetHeight(24)
+VPTCD_FrmDebuffs:SetMovable(true)
+VPTCD_FrmDebuffs:EnableMouse(true)
+VPTCD_FrmDebuffs:RegisterForDrag('LeftButton')
+VPTCD_FrmDebuffs:SetScript('OnDragStart', function() this:StartMoving() end)
+VPTCD_FrmDebuffs:SetScript('OnDragStop',  function() this:StopMovingOrSizing() end)
+VPTCD_FrmDebuffs:Show()
 
-FVPTEST_DebuffList:CreateFontString('FVPTEST_Debuff15', 'OVERLAY', 'GameFontHighlightSmall')
-FVPTEST_Debuff15:SetPoint('TOPLEFT', FVPTEST_DebuffList, 4, -4)
+VPTCD_FrmDebuffs:CreateFontString('FVPTEST_StrKey', 'OVERLAY', 'GameFontHighlightSmall')
+FVPTEST_StrKey:SetPoint('TOPLEFT', VPTCD_FrmDebuffs, 6, -6)
+FVPTEST_StrKey:SetText('Debuffs count =')
 
-FVPTEST_DebuffList:CreateFontString('FVPTEST_Debuff16', 'OVERLAY', 'GameFontHighlightSmall')
-FVPTEST_Debuff16:SetPoint('TOPLEFT', FVPTEST_Debuff15, 'BOTTOMLEFT')
+VPTCD_FrmDebuffs:CreateFontString('FVPTEST_StrVal', 'OVERLAY', 'GameFontHighlightSmall')
+FVPTEST_StrVal:SetPoint('LEFT', FVPTEST_StrKey, 'RIGHT')
+FVPTEST_StrVal:SetText('0')
 
-FVPTEST_DebuffList:CreateFontString('FVPTEST_Debuff17', 'OVERLAY', 'GameFontGreenSmall')
-FVPTEST_Debuff17:SetPoint('TOPLEFT', FVPTEST_Debuff16, 'BOTTOMLEFT')
-
-FVPTEST_DebuffList:CreateFontString('FVPTEST_Debuff18', 'OVERLAY', 'GameFontGreenSmall')
-FVPTEST_Debuff18:SetPoint('TOPLEFT', FVPTEST_Debuff17, 'BOTTOMLEFT')
-
-local f = CreateFrame('FRAME')
-f:RegisterEvent('PLAYER_LOGIN')
-
-f:SetScript('OnEvent', function()
-	MAX_TARGET_DEBUFFS = 40
-	for i = 17, MAX_TARGET_DEBUFFS do
-		local b = CreateFrame("Button", "TargetFrameDebuff" .. i, TargetFrame, "TargetDebuffButtonTemplate")
-		b:SetID(i)
-		b:SetPoint("LEFT", getglobal("TargetFrameDebuff" .. (i - 1)), "RIGHT", 3, 0)
-	end
-
-	local timer = CreateFrame('FRAME')
-	timer.start = GetTime()
-	timer:SetScript('OnUpdate', function()
-		if GetTime() >= (this.start + 1) then
-			this.start = this.start + 1
-                    
-            local debuff, debuffStack, debuffType = UnitDebuff("target", 15)
+local max = 0
+local tick = GetTime()
+VPTCD_FrmDebuffs:SetScript('OnUpdate', function()
+    if GetTime() >= (tick + 1) then
+        tick = tick + 1
+        for i = 1, 40 do
+            local debuff, debuffStack, debuffType = UnitDebuff('target', i)
             if debuff then
-                FVPTEST_Debuff15:SetText('15 - Debuff detect')
-            else
-                FVPTEST_Debuff15:SetText('15')
+                if max < i then
+                    max = i
+                end
             end
-            debuff, debuffStack, debuffType = UnitDebuff("target", 16)
-            if debuff then
-                FVPTEST_Debuff16:SetText('16 - Debuff detect')
-            else
-                FVPTEST_Debuff16:SetText('16')
-            end
-            debuff, debuffStack, debuffType = UnitDebuff("target", 17)
-            if debuff then
-                FVPTEST_Debuff17:SetText('17 - Debuff detect')
-            else
-                FVPTEST_Debuff17:SetText('17')
-            end
-            debuff, debuffStack, debuffType = UnitDebuff("target", 18)
-            if debuff then
-                FVPTEST_Debuff18:SetText('18 - Debuff detect')
-            else
-                FVPTEST_Debuff18:SetText('18')
-            end
-		end
-	end)
+        end
+        FVPTEST_StrVal:SetText(max)
+    end
 end)
